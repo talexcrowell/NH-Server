d2c25c443ffccab21b99ff034d6e207fb84e8df6
talexcrowell <talexcrowell@gmail.com>
2019-04-14 17:57:16 -0700

    basic framework

---
+++ models/user.js
@@ -0,0 +1,36 @@
+'use strict'; 
+
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose'); 
+
+const schema = new mongoose.Schema(
+  { 
+    fullname: { type: String, default: '' }, 
+    username: { type: String, required: true, unique: true }, 
+    password: { type: String, required: true}, 
+  },
+  {timestamps: true}
+); 
+
+schema.set('toJSON', { 
+  virtual: true, 
+  transform: (doc, result) => { 
+    doc._id = result.id;
+    delete result._id; 
+    delete result.__v; 
+    delete result.password; 
+  }
+});
+
+schema.methods.validatePassword = function (pwd) { 
+  const currentUser = this; 
+  return bcrypt.compare(pwd, currentUser.password); 
+}; 
+
+schema.statics.hashPassword = function (pwd) { 
+  return bcrypt.hash(pwd, 10); 
+};
+
+
+
+module.exports = mongoose.model('User', schema); 
---
+++ server.js
@@ -0,0 +1,72 @@
+'use strict'; 
+
+const express = require('express'); 
+const mongoose = require('mongoose'); 
+const morgan = require('morgan'); 
+const passport = require('passport'); 
+const cors = require('cors'); 
+
+const { PORT, CLIENT_ORIGIN, DATABASE_URL } = require('./config'); 
+
+const localStrategy = require('./passport/local'); 
+const jwtStrategy = require('./passport/jwt'); 
+
+const authRouter = require('./routes/auth'); 
+
+const app = express(); 
+mongoose.Promise = global.Promise; 
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: CLIENT_ORIGIN
+  })
+); 
+
+app.use(morgan(process.env.NODE_ENV === 'developement' ? 'dev' : 'common', { 
+  skip: () => process.env.NODE_ENV === 'test'
+})); 
+
+passport.use(localStrategy); 
+passport.use(jwtStrategy); 
+
+const jwtAuth =  passport.authenticate('jwt', { session: false, failWithError: true }); 
+
+app.use('/api/auth', authRouter); 
+
+// Custom 404 Not Found Error Handler
+app.use((req, res, next) => { 
+  const err = new Error('Not Found'); 
+  err.status = 404; 
+  next(err); 
+}); 
+
+// Custom Error Handler
+app.use((err, req, res, next) => { 
+  if (err.status) { 
+    const errBody = Object.assign({}, err, { message: err.message }); 
+    res.status(err.status).json(errBody); 
+  } else { 
+    res.status(500).json({ message: `Server Error: ${err}` }); 
+  }
+}); 
+
+if (require.main === module) {
+  mongoose.connect(DATABASE_URL, {useNewUrlParser: true })
+    .then(instance => { 
+      const conn = instance.connections[0]; 
+      console.info(`Connected to: mongodb://${conn.host}:${conn.port}/${conn.name}`);
+    })
+    .catch(err => { 
+      console.error('Error connecting to MONGO:', err); 
+    }); 
+
+  app.listen(PORT, function () { 
+    console.info(`Server listening on ${this.address().port}`); 
+  }).on('error', err => { 
+    console.error('Error connecting to the SERVER:', err); 
+  }); 
+}
+
+module.exports = app; 
