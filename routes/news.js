0aa131807e10d8d2b0f92a92b730deeacbd48dc7
talexcrowell <talexcrowell@gmail.com>
2019-05-13 11:42:30 -0700

    added all news route

--- routes/news.js
+++ routes/news.js
@@ -21,7 +21,56 @@ router.get('/test', (req, res, next) => {
 });
 
 //retrieving data for general feed
-router.get('/general', (req, res, next) => {
+router.get('/articles', (req, res, next) => {
+  return Promise.all([ 
+    newsapi.v2.topHeadlines({
+      language: 'en',
+      country: 'us',
+      category: 'general',
+      pageSize: 50
+    }),
+    newsapi.v2.topHeadlines({
+      language: 'en',
+      country: 'us',
+      category: 'technology',
+      pageSize: 50
+    }),
+    newsapi.v2.topHeadlines({
+      language: 'en',
+      country: 'us',
+      category: 'science',
+      pageSize: 50
+    }),
+    newsapi.v2.topHeadlines({
+      language: 'en',
+      country: 'us',
+      category: 'business',
+      pageSize: 50
+    }),
+    newsapi.v2.topHeadlines({
+      language: 'en',
+      country: 'us',
+      category: 'health',
+      pageSize: 50
+    })
+  ])
+  //hacky distribution of articles
+    .then(([general, tech, sci, business, health])=> {
+      let output = [];
+      let stdGen = standardizeNewsAPIData(general, 'General');
+      let stdTech = standardizeNewsAPIData(tech, 'Technology');
+      let stdSci = standardizeNewsAPIData(sci, 'Science');
+      let stdBus = standardizeNewsAPIData(business, 'Business');
+      let stdHealth = standardizeNewsAPIData(health, 'Health');
+      output = [...stdGen, ...stdTech, ...stdSci, ...stdBus, ...stdHealth];
+      res.json(output);
+    })
+    .then(data => res.json(data))
+    .catch(err => next(err)); 
+});
+
+//retrieving data for general feed
+router.get('/all', (req, res, next) => {
   return Promise.all([ 
     newsapi.v2.topHeadlines({
       language: 'en',
@@ -56,55 +105,52 @@ router.get('/general', (req, res, next) => {
   ])
   //hacky distribution of articles
     .then(([general, tech, sci, business, health])=> {
+      let newsArr = [];
       let output = [];
-      let stdGen = standardizeNewsAPIData(general, 'general');
-      let stdTech = standardizeNewsAPIData(tech, 'technology');
-      let stdSci = standardizeNewsAPIData(sci, 'science');
-      let stdBus = standardizeNewsAPIData(business, 'business');
-      let stdHealth = standardizeNewsAPIData(health, 'health');
+      let stdGen = standardizeNewsAPIData(general, 'General');
+      let stdTech = standardizeNewsAPIData(tech, 'Technology');
+      let stdSci = standardizeNewsAPIData(sci, 'Science');
+      let stdBus = standardizeNewsAPIData(business, 'Business');
+      let stdHealth = standardizeNewsAPIData(health, 'Health');
+
       let i=0;
       
+      //loading community content into single array
+
       //while loading output response to length (100), check if article has already been loaded before to avoid copies 
-      while(output.length < 100){
+      while(newsArr.length < 100){
         // general news check
-        if (output.filter((article) => stdGen[i].url === article.url).length === 0){
-          output.push(stdGen[i]);
+        if (newsArr.filter((article) => stdGen[i].url === article.url).length === 0){
+          newsArr.push(stdGen[i]);
         }
         // tech news check
-        if (output.filter((article) => stdTech[i].url === article.url).length === 0){
-          output.push(stdTech[i]);
+        if (newsArr.filter((article) => stdTech[i].url === article.url).length === 0){
+          newsArr.push(stdTech[i]);
         }
         // science news check
-        if (output.filter((article) => stdSci[i].url === article.url).length === 0){
-          output.push(stdSci[i]);
+        if (newsArr.filter((article) => stdSci[i].url === article.url).length === 0){
+          newsArr.push(stdSci[i]);
         }
         // business news check
-        if (output.filter((article) => stdBus[i].url === article.url).length === 0){
-          output.push(stdBus[i]);
+        if (newsArr.filter((article) => stdBus[i].url === article.url).length === 0){
+          newsArr.push(stdBus[i]);
         }
         // health news check
-        if (output.filter((article) => stdHealth[i].url === article.url).length === 0){
-          output.push(stdHealth[i]);
+        if (newsArr.filter((article) => stdHealth[i].url === article.url).length === 0){
+          newsArr.push(stdHealth[i]);
         }
         // increase array position for next round
         i += 1;
       }
-      // sort by release date and time
-      output.sort((a,b)=> new Date(b.date + ' '+ b.time) - new Date(a.date + ' '+ a.time));
-      res.json(output);
+      // sort news by release date and time (community already sorted to trending)
+      newsArr.sort((a,b)=> new Date(b.date + ' '+ b.time) - new Date(a.date + ' '+ a.time));
+      
+      //hacky 1:1 add into output array (maybe user patterns?)
+      for(let i=0; i < 75; i++){
+        output.push(newsArr[i]);
+      } 
+      return output;
     })
-    .then(data => res.json(data))
-    .catch(err => next(err)); 
-});
-
-router.get('/technology', (req, res, next) => {
-  newsapi.v2.topHeadlines({
-    language: 'en',
-    country: 'us',
-    category: 'technology',
-    pageSize: 100
-  })
-    .then(results => standardizeNewsAPIData(results))
     .then(data => res.json(data))
     .catch(err => next(err)); 
 });
